b2a4646906c7a457a6c25ad45a1f5467c8de2b33
Sayeef Alam <ecshanto@gmail.com>
2021-06-09 15:34:34 -0400

    Api call to photos on homepage

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -3,15 +3,31 @@ import React,{Component} from 'react';
 
 class Homepage extends Component{
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            albums: []
+        }
+    }
 
-render(){
-    return(
-        <div>
-            <h1>Homepage things</h1>
-        </div>
-    )
 
-}
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/albums/')
+            .then(response => response.json())
+            .then(json => this.setState({photos: json}))
+
+    }
+
+
+    render(){
+        console.log(this.state);
+        return(
+            <div>
+                <h1>Homepage things</h1>
+            </div>
+        )
+
+    }
 
 
 }
